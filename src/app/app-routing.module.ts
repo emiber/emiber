176cacbf9124caea618575326097dd1e6a6179f6
Emiliano Berestovoy <emiliano.berestovoy@accenture.com>
2024-04-08 10:37:47 -0300

    WIP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './views/contact/contact.component';
 import { HomeComponent } from './views/home/home.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
-  { path: 'contact', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
 ];
 
 @NgModule({
